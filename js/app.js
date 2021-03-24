2d4b40504cb088b1fcf8cbcb06a269c3fcfc067a
alejandro medici <el.aleyo@gmail.com>
2021-03-23 21:30:03 -0300

    distintos tipos de for

--- js/app.js
+++ js/app.js
@@ -5,7 +5,25 @@ for (let index = 0; index < 10; index++) {
     
 }
 
+// index++ es equivalente a index = index + 1
+
+for (let index = 0; index < 10; index=index+2) {
+    console.log("Estamos contando el numero es: " + index);   
+}
+
 let myContador = parseInt(prompt("Contar hasta cuanto?"));
 for (let index = 0; index <= myContador; index++) {
+    let unaVariable = "hola" // Esto vive dentro del loop
     console.log("Estamos contando el numero es: " + index);
+}
+
+// Esto es valido pero es un bucle infinito... te rompe todo
+//for(;  ; ){
+
+// no tiene condicion hasta 
+for (let index = 0; ; index++) {
+    console.log("Estamos contando el numero es: " + index);
+    if (index > 10 ) {
+        break;
+    }
 }
