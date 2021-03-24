31836b112556389f6a78b929e9c160bdcbe14dba
alejandro medici <el.aleyo@gmail.com>
2021-03-23 22:04:11 -0300

    while and do while

--- js/while.js
+++ js/while.js
@@ -5,3 +5,11 @@ while( datosEntrada != "ESC"){
     console.log("El usuaario ingreso " + datosEntrada);
     datosEntrada = prompt("Ingrese datos(Si ingresas ESC termina)"); 
 }
+
+
+let numero = 0;
+
+do {
+    numero = prompt("Ingrese numero");
+    console.log(numero);
+} while (parseInt(numero));
